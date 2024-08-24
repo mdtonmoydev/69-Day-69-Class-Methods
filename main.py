83a043f4ab13743b3837656655ddc84a2ae96c80
Md Tonmoy <23169218-MdTonmoy1@users.noreply.replit.com>
2024-08-24 12:52:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Employee:
+  company = "Apple"
+  def show(self):
+    print(f"The name is {self.name} and company is {self.company}")
+
+  @classmethod
+  def changeCompany(cls, newCompany):
+    #classmethod na thakle e1 obj er jonno eta call korle shudhu oi obj er jonno e eta chng hobe. puro class er jonnoi company name chng korte chaile @classmethod use hobe. tokhon e1.changeCompany() dile oi obj er sathe sob obj er jonno e eta chng hobe. and classmethod dile first e instance(self) ney na. instead (cls) ney.
+    cls.company = newCompany
+
+
+e1 = Employee()
+e1.name = "Harry"
+e1.show()
+e1.changeCompany("Tesla")
+e1.show()
+print(Employee.company)
+e2 = Employee()
+e2.name = "Tonmoy"
+e2.changeCompany("Google")
+e2.show()
